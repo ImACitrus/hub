7652ae324cc5944a5bb8f017df5f7e932a062665
ImACitrus <109486300+ImACitrus@users.noreply.github.com>
2023-01-25 11:59:38 +0100

    Added a delay

--- 1962086868.lua
+++ 1962086868.lua
@@ -8,4 +8,5 @@ local Character = Player.Character
 
 local old_pos = Character:GetPivot()
 Character:PivotTo(workspace.tower.sections.finish.FinishGlow.CFrame)
+task.wait(0.2)
 Character:PivotTo(old_pos)
